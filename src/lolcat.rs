f393fab4bdb8b345d60813737184309a89f63ed4
anubhavg-icpl <anubhavg@infopercept.com>
2025-08-28 21:04:31 +0530

    feat: add lolcat binary skeleton - excited to add colors to rust

---
+++ src/lolcat.rs
@@ -0,0 +1,8 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+    
+    println!("Rainbow text coming soon!");
+}
